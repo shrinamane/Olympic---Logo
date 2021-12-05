6f9269d31fe7d51719559927eee4fa293802aa65
Shrina Surve <88468009+shrinamane@users.noreply.github.com>
2021-12-05 11:14:56 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,11 @@
+var canvas= document.getElementById("myCanvas")
+var ctx= canvas.getContext("2d")
+color="red"
+width_of_the_line=1
+var MouseEvent=""
+var last_position_of_x,last_position_of_y
+ctx.beginPath();
+ctx.strokestyle - "red";
+ctx.linewidth - 1
+ctx.rect(150,143,430,200);
+ctx.stroke();
